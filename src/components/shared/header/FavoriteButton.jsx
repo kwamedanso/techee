0d5db1cf68f9deea64810f9b68e18af91dc6c026
kwamedanso <dansokwame96@gmail.com>
2023-12-18 15:36:50 -0600

    Add badge for favorites button

--- src/components/shared/header/FavoriteButton.jsx
+++ src/components/shared/header/FavoriteButton.jsx
@@ -1,12 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { AiOutlineHeart } from "react-icons/ai"
 import "components/shared/header/styles/favorites.css"
 import Favorites from './Favorites'
 import { useLocation } from 'react-router-dom'
+import Context from 'context'
 
 
 export default function FavoriteButton() {
     const [activeFavorites, setActiveFavorites] = useState(false);
+
+    const value = useContext(Context)
+    let { state: { favorites } } = value;
+
     let { pathname } = useLocation();
 
     function handleFavorites() {
@@ -16,12 +21,17 @@ export default function FavoriteButton() {
     useEffect(() => {
         setActiveFavorites(false)
     }, [pathname])
+
+
     return (
         <>
             <div
                 className={`favorite-button ${activeFavorites ? "active" : ""}`}
                 onClick={handleFavorites}>
                 <AiOutlineHeart />
+                {favorites.length > 0 && <div className='notification-badge'>
+                    <span>{favorites.length}</span>
+                </div>}
             </div>
             <Favorites active={activeFavorites} setActiveFavorites={setActiveFavorites} />
             <div
